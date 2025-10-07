66cff0f48d8ab85cf39a0e5e65a9f4459db05571
Graden <graden.gentry@gmail.com>
2025-10-07 17:13:48 -0600

    initial commit

---
+++ LogicGateGame/LogicGateGame macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  LogicGateGame macOS
+//
+//  Created by Graden on 10/7/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
